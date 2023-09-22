2de5e78558f0f7b3b6229d85a31c92ecbbda6cf3
frajavimopu <frajavimopu@gmail.com>
2023-09-22 12:52:18 -0500

    Add getP1 & getP2 methods to Constraint class. Add Point2D.

--- src/tools/Constraint.java
+++ src/tools/Constraint.java
@@ -49,6 +49,26 @@ public class Constraint {
         this.b1 = b1;
     }
 
+    public Point2D getP2() {
+        Point2D p2 = new Point2D();
+        if (a1 != 0.0f && a2 != 0.0f && b1 != 0.0f) {
+            // caso1: P1 (x1=0, ??) & P2(x1=X1MAX, ??)
+            p2.setX1(Coordinate.XMAX);
+            p2.setX2((b1-a1*Coordinate.XMAX)/a2);
+        }
+        return p2;
+    }
+    
+    public Point2D getP1() {
+        Point2D p1 = new Point2D();
+        if (a1 != 0.0f && a2 != 0.0f && b1 != 0.0f) {
+            // caso1: P1 (x1=0, ??) & P2(x1=X1MAX, ??)
+            p1.setX1(0);
+            p1.setX2(b1/a2);
+        }
+        return p1;
+    }
+    
     @Override
     public String toString() {
         return "Constraint{" + "a1=" + a1 + ", a2=" + a2 + ", b1=" + b1 + '}';
